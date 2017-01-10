cbf100f1577c112eb2531a747747d34dab57e7dc
Omar Abdelhafith <o.arrabi@me.com>
2017-01-10 20:51:53 +0000

    Using different environ Adding environ from git Adding license

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Env",
     targets: [
-      Target(name:"Environ"),
-      Target(name:"Env", dependencies: ["Environ"]),
+      Target(name:"Env"),
+    ],
+    dependencies: [
+      .Package(url: "https://github.com/oarrabi/Environ.git", majorVersion: 0)
     ]
 )
--- Sources/Env/Env.swift
+++ Sources/Env/Env.swift
@@ -8,47 +8,47 @@ import Environ
 
 
 public enum Env {
-  
+
   /// Return the list of all the enviromenment keys, vlues pairs passed to the script
   public static var items: [String: String] {
-    
+
     var items: [String: String] = [:]
     guard var environCopy = environ else { return items }
-    
+
     while true {
       let x = environCopy[0]
       guard let (key, value) = splitEnvironment(x) else { return items }
       items[key] = value
-      
+
       environCopy = environCopy.advanced(by: 1)
     }
   }
-  
+
   /// Return the list of all the enviromenment keys passed to the script
   public static var keys: [String] {
     return Array(items.keys)
-  }  
-  
+  }
+
   /// Return the list of all the enviromenment values passed to the script
   public static var values: [String] {
     return Array(self.items.values)
   }
-  
+
   /**
    Return the enviromenment for the provided key
-   
+
    - parameter key: The enviromenment variable key
-   
+
    - returns: The enviromenment variable value
    */
   public static func get(_ key: String) -> String? {
     guard let value = getenv(key) else { return nil }
     return String(cString: value)
   }
-  
+
   /**
    Set a new value for the enviromenment variable
-   
+
    - parameter key: The enviromenment variable key
    - parameter value: The enviromenment variable value
    */
@@ -59,8 +59,8 @@ public enum Env {
       unsetenv(key)
     }
   }
-  
-  
+
+
   /**
    Clear all the enviromenment variables
    */
@@ -68,59 +68,59 @@ public enum Env {
     self.keys
       .forEach{ self.set($0, nil) }
   }
-  
+
   /**
    Check if the enviromenment variable key exists
-   
+
    - parameter key: The enviromenment variable key
-   
+
    - returns: true if exists false otherwise
    */
   public static func hasKey(_ key: String) -> Bool {
     return self.keys.contains { $0 == key }
   }
-  
-  
+
+
   /**
    Check if the enviromenment variable value exists
-   
+
    - parameter key: The enviromenment variable value
-   
+
    - returns: true if exists false otherwise
    */
   public static func hasValue(_ value: String) -> Bool {
     return self.values.contains(value)
   }
-  
+
   /**
    Iterate through the list of enviromenment variables
-   
+
    - parameter callback: callback to call on each key/value pair
    */
   public static func eachPair(_ callback: (_ key: String, _ value: String) -> ()) {
     zip(self.keys, self.values).forEach(callback)
   }
-  
+
 }
 
 
 extension Env {
-  
+
   fileprivate static func splitEnvironment(_ environment: UnsafePointer<Int8>?) -> (String, String)? {
-    
+
     guard
       let environment = environment
       else { return nil }
-    
+
     let environmentStr = String(cString: environment)
-    
+
     guard
       let index = environmentStr.characters.index(of: "=")
       else { return nil }
-    
+
     let key = environmentStr[environmentStr.startIndex..<index]
-    
-    let advanced = environmentStr.index(after: index) 
+
+    let advanced = environmentStr.index(after: index)
     let value = environmentStr[advanced..<environmentStr.endIndex]
     return (key, value)
   }
